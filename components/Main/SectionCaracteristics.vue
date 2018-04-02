3f6bfa5006db56df04a05c6396284544c17d2caf
juanhosk <smrjuanjomovil@gmail.com>
2018-04-02 15:42:03 +0200

    added contact form

--- components/Main/SectionCaracteristics.vue
+++ components/Main/SectionCaracteristics.vue
@@ -33,21 +33,34 @@
               </v-flex>
               <v-flex xs12 md4>
                 <v-card class="elevation-0 transparent">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3028.347583637035!2d-3.9112046840852663!3d40.62221597934174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd417763abbb2a13%3A0x530fddb5df90269d!2sLa+Bodeguita!5e0!3m2!1ses!2ses!4v1522077431457" width="550" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3028.347583637035!2d-3.9112046840852663!3d40.62221597934174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd417763abbb2a13%3A0x530fddb5df90269d!2sLa+Bodeguita!5e0!3m2!1ses!2ses!4v1522077431457" style="width: 100%; height: 300px; border: 0;" allowfullscreen></iframe>
                 </v-card>
               </v-flex>
               <v-flex xs12 md4>
                 <v-card class="elevation-0 transparent">
-                  <v-card-text class="text-xs-center">
-                    <v-icon x-large class="blue--text text--lighten-2">text_format</v-icon>
-                  </v-card-text>
                   <v-card-title primary-title class="layout justify-center">
                     <div class="headline text-xs-center">Formulario de contacto</div>
                   </v-card-title>
                   <v-card-text>
-                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                    <v-form v-model="valid">
+                      <v-text-field
+                        label="Nombre"
+                        v-model="name"
+                        :rules="nameRules"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        label="E-mail"
+                        v-model="email"
+                        :rules="emailRules"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        name="text"
+                        label="Escribe tu consulta"
+                        multi-line
+                      ></v-text-field>
+                    </v-form>
                   </v-card-text>
                 </v-card>
               </v-flex>
@@ -58,3 +71,19 @@
     </section>
   </div>
 </template>
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      name: '',
+      nameRules: [
+        v => !!v || 'Campo obligatorio'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'Campo obligatorio'
+      ],
+      text: ''
+    })
+  }
+</script>

6c4fd4240ecf0381ae9e7f583e45e76bb261723e
Garlos <gaarlos@users.noreply.github.com>
2018-04-02 15:42:27 +0200

    Styles

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,12 +1,29 @@
 <template>
-  <v-toolbar app fixed>
+  <v-toolbar fixed flat v-scroll="onScroll" id="top-bar">
     <v-toolbar-side-icon></v-toolbar-side-icon>
     <v-toolbar-title>Title</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link One {{ offsetTop }}</v-btn>
       <v-btn flat>Link Two</v-btn>
       <v-btn flat>Link Three</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
+<script>
+  export default {
+    data: () => ({
+      offsetTop: 0
+    }),
+    methods: {
+      onScroll (e) {
+        this.offsetTop = window.pageYOffset || document.documentElement.scrollTop
+      }
+    },
+    watch: {
+      offsetTop () {
+        document.getElementById('top-bar').style.backgroundColor = this.offsetTop <= 100 ? 'transparent' : '#dc3250'
+      }
+    }
+  }
+</script>

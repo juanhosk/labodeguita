fe0461909b76182286d15032f9827f34d00c45d5
Garlos <gaarlos@users.noreply.github.com>
2018-04-02 19:21:52 +0200

    Fixed express issue

--- server/index.js
+++ server/index.js
@@ -1,36 +1,30 @@
 const express = require('express')
 const { Nuxt, Builder } = require('nuxt')
+
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
 const messagesRoutes = require('./routes/Messages')
 
-app.set('port', port)
-
-// Import and Set Nuxt.js options
+// Import and set Nuxt.js options
 let config = require('../nuxt/nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
-async function start () {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+// Init Nuxt.js
+const nuxt = new Nuxt(config)
 
-  // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  }
-
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
-
-  // routes
-  app.use('/messages', messagesRoutes)
-
-  // Listen the server
-  app.listen(port, host)
-  console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
+// Start build process in dev mode
+if (config.dev) {
+  const builder = new Builder(nuxt)
+  builder.build()
 }
 
-start()
+// routes
+app.use('/messages', messagesRoutes)
+
+// Give nuxt middleware to express
+app.use(nuxt.render)
+
+// Start express server
+app.listen(port, host)
--- server/routes/Messages.js
+++ server/routes/Messages.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', (req, res) => {
   res.json({
